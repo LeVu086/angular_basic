2cb120ee0d6f3e7e5a09f7b4b7f0f63eda626c88
levu086 <levu086it@gmail.com>
2020-01-06 20:39:26 +0700

    test component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `<h1>Heleo PROJECT BASIC </h1>
+              <app-hello></app-hello>
+              <app-hi></app-hi>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
---
+++ src/app/components/hello/hello.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-hello',
+  template: `<h2>hello le vuong vu</h2>`
+})
+export class HelloComponent{}
